67adab8eef908c5e12ec5e6a65214797af019265
William <322969@tldsb.me>
2022-11-28 11:19:06 -0500

    Nov 28th tweaking

--- src/Utils/Constants.java
+++ src/Utils/Constants.java
@@ -3,8 +3,14 @@ package Utils;
 import java.util.HashMap;
 
 public class Constants {
-  public static int test1 = 4;
-  public static HashMap<String, String> inventory = new HashMap<String, String>();
+  
   public static boolean gameState = true;
+
+  public static HashMap<String, String> inventory = new HashMap<String, String>();
+
+  //Player stats will be here
+  //Values not finalized
+  public static int playerHealth = 10;
+  public static double playerDamageMultiplier = 1.1;
   
 }
