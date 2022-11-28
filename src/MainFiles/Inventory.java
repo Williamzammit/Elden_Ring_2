1ffef388eed2f33ef59e8fd11dc41d77de067c7f
William <322969@tldsb.me>
2022-11-28 10:44:49 -0500

    First part of Inventory system complete

--- src/MainFiles/Inventory.java
+++ src/MainFiles/Inventory.java
@@ -1,5 +1,35 @@
 package MainFiles;
 
+import static Utils.Constants.*;
+
 public class Inventory {
+    
+    public Inventory(){
+        for (int i = 1; i <= 10; i++){
+            inventory.put("Slot" + i, "Empty");
+        }
+    }
+
+    public void viewInventory(){
+        for(Object i:inventory.values()){
+        System.out.println(i);
+        }
+    }
+
+    public void addItem(String newItem){
+        for(int i = 1; i <= 10; i++){
+            if(inventory.get("Slot" + i) == "Empty"){
+                inventory.put("Slot" + i, newItem);
+                i = 11;
+            }
+            if(i == 10 && inventory.get("Slot" + i) != "Empty"){
+                System.out.println();
+            }
+        }
+        
+
+                
+        
+    }
     
 }
