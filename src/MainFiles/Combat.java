c3e312e45fc44a534c814f011fdb63a6c5ede1fb
Aiden <235213@tldsb.me>
2022-12-02 12:22:15 -0500

    stuff to work at home

--- src/MainFiles/Combat.java
+++ src/MainFiles/Combat.java
@@ -1,5 +1,16 @@
 package MainFiles;
 
+
 public class Combat {
-   
+    Entities selectedEnemy;
+    Entities[][] enemies = { {new Entities("FortniteMan", 10, 5), new Entities("nuts", 20, 1), new Entities("lol", 7, 2)},{},{},{},{}};
+    public Entities getEnemy() {
+        
+    selectedEnemy = enemies[0][(int) (Math.random()*2)];
+
+        return selectedEnemy;
+    }
+    public void initiateCombat(Entities enemy) {
+        System.out.println("\033[1n" + enemy.name + "\033[0m" + "\n" + "Health: " + enemy.health + "\n" + "Damage:" + enemy.damage);
+   }
 }
---
+++ src/MainFiles/Entities.java
@@ -0,0 +1,22 @@
+package MainFiles;
+
+public class Entities  {
+    int health, damage;
+    String name;
+
+    public Entities(String name, int health, int damage){
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
+        
+        
+        
+    }
+
+    /*public void Player(String name, int health, int damage){
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
+    }*/
+    
+}
